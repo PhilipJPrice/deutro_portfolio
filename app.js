577ba3700dc193c9ef2e26e8dc3949ddcb74e238
Philip Price <philipprice@Philips-MacBook-Pro-2.local>
2026-06-05 12:09:48 -0700

    Feat: Chess Page, Gems Page, Divine Page, and Pendulum Page

--- app.js
+++ app.js
@@ -15,4 +15,21 @@ app.get('/', (req, res) => {
 app.get('/animations/birthday', (req, res) => {
     res.render('animations/birthday', { title: 'Birthday' });
 })
+
+app.get('/animations/divine', (req, res) => {
+    res.render('animations/divine', { title: 'Divine' });
+})
+
+app.get('/animations/chess', (req, res) => {
+    res.render('animations/chess', { title: 'Chess' });
+})
+
+app.get('/animations/gems', (req, res) => {
+    res.render('animations/gems', { title: 'Gems' });
+})
+
+app.get('/animations/pendulum', (req, res) => {
+    res.render('animations/pendulum', { title: 'Pendulum' });
+})
+
 app.listen(3000, () => console.log('Server running on port 3000'));
